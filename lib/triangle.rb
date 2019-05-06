b9a41e58c59e0d6b368f54b065db9bb27ccdaadf
Ethan Sagin <ethansagin@gmail.com>
2019-05-06 15:21:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   
   def kind
     tri_array = [side1, side2, side3]
-    if tri_array.inject { |product, n| product * n} == 0 || 
+    if tri_array.inject { |product, n| product * n} <= 0 || tri_array.inject { |sum, n| sum + n} < 
       ERROR
     elsif tri_array.uniq.length == 1
       :equilateral
